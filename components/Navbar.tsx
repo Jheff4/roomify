c6532eb9b6f72a17bf055f089f595cdcf16ed3d2
Jheff4 <ogbevoenetinosa@gmail.com>
2026-03-03 19:58:32 +0100

    added puter auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,25 +3,25 @@ import Button from "./ui/Button";
 import { useOutletContext } from "react-router";
 
 const Navbar = () => {
-  // const { isSignedIn, userName, signIn, signOut } = useOutletContext<AuthContext>()
+  const { isSignedIn, userName, signIn, signOut } = useOutletContext<AuthContext>()
 
-  // const handleAuthClick = async () => {
-  //   if (isSignedIn) {
-  //     try {
-  //       await signOut();
-  //     } catch (e) {
-  //       console.error(`Puter sign out failed: ${e}`);
-  //     }
+  const handleAuthClick = async () => {
+    if (isSignedIn) {
+      try {
+        await signOut();
+      } catch (e) {
+        console.error(`Puter sign out failed: ${e}`);
+      }
 
-  //     return;
-  //   }
+      return;
+    }
 
-  //   try {
-  //     await signIn();
-  //   } catch (e) {
-  //     console.error(`Puter sign in failed: ${e}`);
-  //   }
-  // };
+    try {
+      await signIn();
+    } catch (e) {
+      console.error(`Puter sign in failed: ${e}`);
+    }
+  };
 
   return (
     <header className="navbar">
@@ -44,25 +44,25 @@ const Navbar = () => {
         </div>
 
         <div className="actions">
-          {/* {isSignedIn ? ( */}
-          <>
-            <span className="greeting">
-              {/* {userName ? `Hi, ${userName}` : 'Signed in'} */}
-            </span>
+          {isSignedIn ? (
+            <>
+              <span className="greeting">
+                {userName ? `Hi, ${userName}` : 'Signed in'}
+              </span>
 
-            {/* <Button size="sm" onClick={handleAuthClick} className="btn">
+              <Button size="sm" onClick={handleAuthClick} className="btn">
                 Log Out
-              </Button> */}
-          </>
-          {/* ) : ( */}
-          <>
-            {/* <Button onClick={handleAuthClick} size="sm" variant="ghost">
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button onClick={handleAuthClick} size="sm" variant="ghost">
                 Log In
-              </Button> */}
+              </Button>
 
-            <a href="#upload" className="cta">Get Started</a>
-          </>
-          {/* )} */}
+              <a href="#upload" className="cta">Get Started</a>
+            </>
+          )}
         </div>
       </nav>
     </header>
